39da4fad467653623cd5812a4d65ddac67f97987
Michael Kreil <github@michael-kreil.de>
2023-11-01 23:53:16 +0100

    write results

--- bin/3_get_metadata.js
+++ bin/3_get_metadata.js
@@ -3,7 +3,7 @@
 
 import 'work-faster';
 import { spawn } from 'child_process';
-import { resolve } from 'path';
+import { basename, resolve } from 'path';
 import { existsSync, mkdirSync, readFileSync, readdirSync, statSync, writeFileSync } from 'fs';
 import { Progress } from './lib.js';
 
@@ -17,7 +17,7 @@ const files = [];
 console.log('scan:');
 readdirSync(srcPath).forEach(topic => {
 	let path = resolve(srcPath, topic);
-	if (!statSync(path).isDirectory) return;
+	if (!statSync(path).isDirectory()) return;
 	console.log('   - ' + topic);
 	readdirSync(path).forEach(filename => {
 		if (!filename.endsWith('.jsonl.xz')) return;
@@ -29,42 +29,80 @@ readdirSync(srcPath).forEach(topic => {
 
 files.sort(() => Math.random() - 0.5);
 
-let progress = Progress();
 let sizePos = 0;
 let sizeSum = files.reduce((s, f) => s + f.size, 0);
-progress.update(0);
 console.log(`process ${files.length} files with ${(sizeSum / 0x40000000).toFixed(1)} GB:`);
-await files.forEachAsync(async (file, i) => {
-	let tempFilename = resolve(tmpPath, file.filename.replace(/\.json.*/, '.json'));
-
-	if (existsSync(tempFilename)) {
-		file.data = JSON.parse(readFileSync(tempFilename));
-	} else {
-		file.data = await new Promise(res => {
-			const child = spawn(
-				'bash',
-				['-c', `cat "${file.fullname}" | xz -d | node _analyse_tweets.js`]
-			);
-			const buffers = [];
-			child.stdout.on('data', chunk => buffers.push(chunk));
-			child.stderr.on('data', chunk => console.log(String(chunk)));
-			child.on('error', (...args) => {
-				console.log(args);
-				console.log({ file });
-				process.exit();
-			});
-			child.on('close', code => {
-				if (code !== 0) throw Error();
-				res(JSON.parse(Buffer.concat(buffers)));
-			});
-		})
-		writeFileSync(tempFilename, JSON.stringify(file.data));
-	}
-
+let progress = Progress();
+await files.forEachAsync(async file => {
+	file.content = await analyseContent(file.fullname);
 	sizePos += file.size;
 	progress.update(sizePos / sizeSum);
 }, 8)
 
 progress.finish();
 
-console.log(files);
+console.log('save data');
+
+const topics = new Map();
+files.forEach(file => {
+	let topic = topics.get(file.topic);
+	if (!topic) {
+		topic = [];
+		topics.set(file.topic, topic);
+	}
+	topic.push({
+		filename: file.filename,
+		size_compressed: file.size,
+		size_uncompressed: file.content.uncompressed_size,
+		tweet_count: file.content.tweet_count,
+		reply_count: file.content.reply_count,
+		retweet_count: file.content.retweet_count,
+	})
+})
+
+Array.from(topics.entries()).forEach(([topic, files]) => {
+	files.sort((a, b) => a.filename < b.filename ? -1 : 1);
+	const filename = resolve(srcPath, 'filelist-' + topic);
+	writeFileSync(filename + '.json', JSON.stringify(files));
+	writeFileSync(filename + '.csv', csv(files));
+})
+
+function csv(data) {
+	const keys = Array.from(Object.keys(data[0]));
+	data = data.map(entry => keys.map(k => entry[k]));
+	data.unshift(keys);
+	return data.map(entry => entry.join(',') + '\n').join('');
+}
+
+async function analyseContent(fullname) {
+	let tempFilename = resolve(tmpPath, basename(fullname).replace(/\.json.*/, '.json'));
+
+	if (existsSync(tempFilename)) {
+		return JSON.parse(readFileSync(tempFilename));
+	}
+
+	const data = await new Promise(res => {
+		const child = spawn(
+			'bash',
+			['-c', `cat "${fullname}" | xz -d | node _analyse_tweets.js`]
+		);
+		const buffers = [];
+		child.stdout.on('data', chunk => buffers.push(chunk));
+		child.stderr.on('data', chunk => console.log(String(chunk)));
+		child.on('error', (...args) => {
+			console.log(args);
+			console.log({ fullname });
+			process.exit();
+		});
+		child.on('close', code => {
+			if (code !== 0) {
+
+				console.log({ fullname });
+				throw Error();
+			}
+			res(JSON.parse(Buffer.concat(buffers)));
+		});
+	})
+	writeFileSync(tempFilename, JSON.stringify(data));
+	return data;
+}
